462f9fed83f596af47892ac0941446ae83a0d3c6
SmithBWare <Smithwrestling89@gmail.com>
2020-06-28 11:48:55 -0400

    set up initial questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,46 @@
 // THEN the game is over
 // WHEN the game is over
 // THEN I can save my initials and score
+
+var Questions = [
+    {
+        question: "Which is not a valid Javascript data type?",
+        option1: "A string",
+        option2: "An array",
+        option3: "A boolean",
+        option4: "A function",
+        answer: "A function"
+    },
+    {
+        question: "Console.log() is considered a _______?"
+        option1: "Method",
+        option2: "Function",
+        option3: "Object",
+        option4: "String",
+        answer: "Method"
+    },
+    {
+        question: "What is the name of the process for joining two strings?"
+        option1: "String join",
+        option2: "Stringify",
+        option3: "Concatenation",
+        option4: "Escape",
+        answer: "Concatenation"
+    },
+    {
+        question: "How do we declare variables in Javascript?",
+        option1: "var",
+        option2: "const",
+        option3: "let",
+        option4: "All of the above",
+        answer: "All of the above"
+    },
+    {
+        question: "Which of the follow code options generates a window for user input?",
+        option1: "window('hello');",
+        option2: "console.log('Hello')",
+        option3: "localStorage.setItem('Hello');",
+        option4: "prompt('Hello?');",
+        answer: "prompt('Hello');"
+    },
+];
